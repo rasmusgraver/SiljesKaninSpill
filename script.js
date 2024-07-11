cf1307591c3d0dc21e0c341741e8fe8c4b8159d6
Rasmus Graver <rasmus.graver@gmail.com>
2024-07-11 22:00:18 +0200

    format script js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 function info() {
-    document.getElementById("info").classList.toggle("show")
+  document.getElementById("info").classList.toggle("show")
 }
 
 const navninput = document.getElementById("navninput")
-navninput.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        const navn = navninput.value.trim()
-        localStorage.setItem("navn", navn)
-    }
+navninput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault()
+    const navn = navninput.value.trim()
+    localStorage.setItem("navn", navn)
+  }
 })
 
 // Hent opp lagret navn fra localStorage:
 const navnLagret = localStorage.getItem("navn")
 if (navnLagret) {
-    navninput.value = navnLagret
+  navninput.value = navnLagret
 }
-
