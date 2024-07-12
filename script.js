b7af6f0143ab5b7b120d55ce9548918a0d91fafc
Rasmus Graver <rasmus.graver@gmail.com>
2024-07-12 11:50:01 +0200

    Vis skjul knapper logikk med hjemknapp

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const navninput = document.getElementById("navninput")
 const matdiv = document.getElementById("mat")
 const klaerdiv = document.getElementById("klaer")
+const infoknapp = document.getElementById("infoknapp")
+const hjemknapp = document.getElementById("hjemknapp")
+const saldoElm = document.getElementById("saldo")
 
 // Hent opp lagret navn fra localStorage:
 const navnLagret = localStorage.getItem("navn")
 if (navnLagret) {
   navninput.value = navnLagret
+}
+
+let saldo = parseInt(localStorage.getItem("saldo")) || 100
+oppdaterSaldo(saldo)
+
+function oppdaterSaldo(saldo) {
+  saldoElm.innerHTML = saldo
+  localStorage.setItem("saldo", saldo)
+}
+
+function tjenpenger() {
+  saldo += 100
+  oppdaterSaldo(saldo)
 }
 
 navninput.addEventListener("keypress", function (event) {
@@ -16,26 +32,41 @@ navninput.addEventListener("keypress", function (event) {
   }
 })
 
-function info() {
-  skjulAside()
+function info(skjul = false) {
+  visSkjulAside(skjul ? "block" : "none")
   document.getElementById("info").classList.toggle("show")
 }
 
-function skjulAside() {
+function visSkjulAside(display = "none") {
   const asides = document.querySelectorAll("aside")
   for (let aside of asides) {
-    aside.style.display = "none"
+    aside.style.display = display
+  }
+  if (display == "none") {
+    infoknapp.style.display = "none"
+    hjemknapp.style.display = "block"
+    navninput.style.display = "none"
+  } else {
+    infoknapp.style.display = "block"
+    hjemknapp.style.display = "none"
+    navninput.style.display = "block"
   }
 }
 
+function hjem() {
+  visSkjulAside("block")
+  matdiv.style.display = "none"
+  klaerdiv.style.display = "none"
+}
+
 function mat() {
-  skjulAside()
+  visSkjulAside()
   matdiv.style.display = "flex"
   klaerdiv.style.display = "none"
 }
 
 function klaer() {
-  skjulAside()
+  visSkjulAside()
   matdiv.style.display = "none"
   klaerdiv.style.display = "flex"
 }
