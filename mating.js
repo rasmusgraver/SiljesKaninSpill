586bd394eca4a6720483de194f5f6f73270540ba
Rasmus Graver <rasmus.graver@gmail.com>
2024-07-13 12:29:39 +0200

    Dropped dragEnd

--- mating.js
+++ mating.js
@@ -8,63 +8,39 @@ const matElms = document.querySelectorAll("#mat > div > img")
 for (let elm of matElms) {
   elm.addEventListener("touchstart", dragStart)
   elm.addEventListener("touchmove", dragMove)
-  elm.addEventListener("touchend", dragEnd)
+  // elm.addEventListener("touchend", dragEnd)
   elm.addEventListener("dragstart", dragStart)
   elm.addEventListener("drag", dragMove)
-  elm.addEventListener("mouseup", dragEnd)
+  // elm.addEventListener("mouseup", dragEnd)
 }
 
 function dragStart(event) {
-  spiseElm = event.target
-  spiseElm.style.width = "50px"
-  spiseElm.style.position = "fixed"
+  if (spiseElm == null) {
+    spiseElm = event.target
+    spiseElm.style.width = "50px"
+    spiseElm.style.position = "fixed"
+  }
 }
 
 function dragMove(event) {
   // spiseElm = event.target
+  if (spiseElm == null) {
+    return
+  }
   let clientX = null
   let clientY = null
   if (typeof event.changedTouches != "undefined") {
     // Touch event:
     clientX = event.changedTouches[0].clientX
     clientY = event.changedTouches[0].clientY
-  } else {
+  } else if (typeof event.clientX != "undefined") {
     // Click event:
-    if (typeof event.clientX != "undefined") {
-      clientX = event.clientX
-      clientY = event.clientY
-    } else {
-      // Click event, men uten clientX => dragEnd!
-      dragEnd()
-    }
+    clientX = event.clientX
+    clientY = event.clientY
   }
   if (clientX) {
     spiseElm.style.left = clientX - 25 + "px"
     spiseElm.style.top = clientY - 25 + "px"
-  }
-}
-
-function dragEnd(event) {
-  // spiseElm = event.target
-
-  let clientX = null
-  let clientY = null
-  if (typeof event.changedTouches != "undefined") {
-    // Touch event:
-    clientX = event.changedTouches[0].clientX
-    clientY = event.changedTouches[0].clientY
-  } else {
-    // Click event:
-    if (typeof event.clientX != "undefined") {
-      clientX = event.clientX
-      clientY = event.clientY
-    } else {
-      // Click event, men uten clientX => dragEnd!
-      dragEnd()
-    }
-  }
-  if (clientX) {
-    console.log("DRAG END", clientX, clientY)
     const distX = clientX - targetX
     const distY = clientY - targetY
     const dist = Math.sqrt(distX ** 2 + distY ** 2)
@@ -74,11 +50,10 @@ function dragEnd(event) {
   }
 }
 
-function getXandY() {
-  // TODO: Refactor
-}
-
 function spis() {
+  if (spiseElm == null) {
+    return
+  }
   kaninElm.src = "bilder/munn.png"
   spiseElm.style.width = "20px"
   spiseElm.style.left = targetX - 10 + "px"
@@ -87,6 +62,9 @@ function spis() {
 }
 
 function spisFerdig() {
+  if (spiseElm == null) {
+    return
+  }
   kaninElm.src = "bilder/kanin.png"
   // Reset elementet som blir spist:
   spiseElm.style.left = null
